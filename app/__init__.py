f77716a0d81e24f201681e504226813b80ee1e42
HuangHT1999-WHU <huanght1999@qq.com>
2025-11-27 20:13:57 +0800

    优化项目架构

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+
+import os
+
+# 初始化数据库
+db = SQLAlchemy()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__, root_path=os.path.abspath('.'))
+    app.config.from_object(config_class)
+
+    # 初始化扩展
+    db.init_app(app)
+
+    # 注册蓝图
+    from app.routes.main import main as main_bp
+    from app.routes.api import api as api_bp
+    app.register_blueprint(main_bp)
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
